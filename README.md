d2d76edc86269c8b6b7dde8f5e696c2a8f63c64a
ZelikSV <zeliksv@gmail.com>
2021-04-19 17:51:56 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#  TODO List
+#Technology stack: 
+  - [React]
+  - [Webpack]
+  - [Typescript]
+  - [Material UI]
+  - [Jest]
+
+# To run app in development mode
+1. `npm i`
+2. `npm start`
+
+# To run app in prod mode
+1. `npm i`
+2. `npm build`
---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant="h5" component="h2">
+        Hello React App
+      </Typography>
+    </div>
+  );
+};
+
+export default App;
